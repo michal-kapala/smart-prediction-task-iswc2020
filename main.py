5993adfec0db7bcba6ffc8521a9ab7d0a8830fe8
michal-kapala <mim4k1@gmail.com>
2022-01-28 05:38:08 +0100

    Sparql queries
    yay

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ table = str.maketrans('','', string.punctuation)
 
 # ETL
 dataset_df = load_dataset(dataset_file, is_dbpedia)
-# For performance reasons only
-dataset_df = dataset_df.head(100)
+# For performance and wikidata api 429 'Too Many Requests' response reasons
+dataset_df = dataset_df.head(1)
 categories = load_wikidata_categories()
 
 # Question text preprocessing
@@ -74,7 +74,7 @@ for index, row in dataset_df.iterrows():
                 # Search through category descriptions
                 for index2, cat_row in categories.iterrows():
                     # If the candidate is found in the description, save the qitem id
-                    if cat_row['category'].find(c) or cat_row['category'].find(c.lower()):
+                    if cat_row['category'].find(c) > -1 or cat_row['category'].find(c.lower()) > -1:
                         qitems.append(cat_row['wiki_id'])
         # Remove duplicate ids for the same question
         qitems = list(dict.fromkeys(qitems))
@@ -83,10 +83,28 @@ for index, row in dataset_df.iterrows():
     else:
         qitems_column.append([])
 
-dataset_df['cand_types'] = qitems_column
+dataset_df['cand_qitems'] = qitems_column
 print(dataset_df)
 
+# SPARQL queries
+cand_types_column = []
+for index, row in dataset_df.iterrows():
+    if row['cand_qitems'] != []:
+        cand_types = []
+        for id in row['cand_qitems']:
+            # A single query can return multiple matches
+            res = get_types_for_entity_id(sparql, id)
+            cand_types.extend(res)
+        cand_types = list(dict.fromkeys(cand_types))
+        cand_types_column.append(cand_types)
+    else:
+        cand_types_column.append([])
 
+dataset_df['cand_types'] = cand_types_column
+
+print(dataset_df)
+
+dataset_df.to_json(r'.\results.json')
 # Query examples
 
 # Query for entities which are 'instance-of' of type_id
@@ -96,8 +114,6 @@ print(dataset_df)
 # Query for entity types of an object specified by the url
 # Example url: http://www.wikidata.org/entity/Q34172
 # print(get_types_for_entity_url(sparql, 'http://www.wikidata.org/entity/Q34172'))
-
-
 
 
 
@@ -126,6 +142,6 @@ print(dataset_df)
 # 
 # prediction = clf.predict(Xt)
 #
-#rounded_prediction = np.argmax(prediction, axis=1)
-#rounded_test = np.argmax(yt, axis=1)
-#print(accuracy_score(rounded_test, rounded_prediction))
+# rounded_prediction = np.argmax(prediction, axis=1)
+# rounded_test = np.argmax(yt, axis=1)
+# print(accuracy_score(rounded_test, rounded_prediction))
